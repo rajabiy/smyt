f1979695fe01d38e73bbb1a5bfdd5906a483f38f
unknown <amin@iybmbserv.samarkand.bankipakyuli.uz>
2014-06-09 18:20:22 +0500

    30 мин

--- smyt/dclass/models.py
+++ smyt/dclass/models.py
@@ -13,5 +13,11 @@ for key, value in yaml_models.iteritems():
     model = gen_model(key, value)
     admin.site.register(model)
     models_dict[model._meta.verbose_name] = model
-    models_fields[model._meta.verbose_name] = value.get('fields')
+
+    fields = {'id': {'title': 'id', 'type': 'int'}}
+
+    for field in value.get('fields'):
+        fields[field.get('id')] = field
+
+    models_fields[model._meta.verbose_name] = fields
 
--- smyt/dclass/views.py
+++ smyt/dclass/views.py
@@ -2,7 +2,7 @@ from rest_framework.views import APIView
 from django.forms.models import model_to_dict
 # Create your views here.
 
-from dclass.models import models_dict
+from dclass.models import models_dict, models_fields
 from rest_framework.response import Response
 
 
@@ -16,28 +16,19 @@ class ModelList(APIView):
 
 class Model(APIView):
     def get(self, request, fomat=None, model=None):
-        data = None
+        data = []
         if model:
             chosed_model = models_dict[model]
-            print type(chosed_model._meta.fields[0].__class__)
-            data = chosed_model.objects.all().values()
+            data.append(models_fields[model])
+            data.append(chosed_model.objects.all().values())
         return Response(data)
 
 
 class ModelDetails(APIView):
     def get(self, request, pk, fomat=None, model=None):
-        data = None
+        data = []
         if model:
             chosed_model = models_dict[model]
-            data = model_to_dict(chosed_model.objects.get(pk=pk))
+            data.append(models_fields[model])
+            data.append(model_to_dict(chosed_model.objects.get(pk=pk)))
         return Response(data)
-
-
-class ModelDetails(APIView):
-    def get(self, request, pk, fomat=None, model=None):
-        if model:
-            chosed_model = models_dict[model]
-            data = type(chosed_model._meta.fields[0])
-        return Response(data)
-
-
